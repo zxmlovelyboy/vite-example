02bcc4bafdb427493eafa416a5fa48a7f13149bf
simonzhu <383980673@qq.com>
2021-01-30 18:06:03 +0800

    submit code

---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App" @sendMsg="handler"/>
+    <h3>我的年龄：{{ myAge }}</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from 'vue'
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  setup () {
+    var myAge = ref(0)
+    const count = ref(1)
+    const handler = value => {
+      console.log('子组件emit传值到父页面：', value)
+      myAge.value = value + (count.value++)
+    }
+    debugger
+    return { myAge, handler }
+  }
+}
+</script>
